b3cad46e422e506795bdc78590bfabc25fff67d6
ls zr <1085963811@qq.com>
2018-03-21 21:23:00 +0800

    update

---
+++ ClionProjects/Pat/cmake-build-debug/CMakeFiles/Pat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pat.dir/main.cpp.o"
+  "Pat.pdb"
+  "Pat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ClionProjects/Pat/saveFile.sh
@@ -0,0 +1,5 @@
+fileName=$1
+cp main.cpp filename
+git add .
+git commit -m "update"
+git push
